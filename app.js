5f20c7e74d3a1f6a7035faa042038aa4a2baf411
d-akhil-kumar <akillcoder@gmail.com>
2021-07-19 00:24:45 +0530

    cors, err middleware, logger, router

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const helmet = require('helmet')
+const cors = require('cors')
+const router = require('./routes/router')
+const reqLogger = require('./middlewares/reqLogger')
+const errMiddleware = require('./middlewares/error')
 require('dotenv').config()
 
 app = express()
 
 app.use(helmet())
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true,
 }));
 
+app.use(reqLogger)
+app.use('/', router)
+
+app.use(errMiddleware)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
