823702ff78d1c51604d02ecbc8c39fe8b93b22e1
d-akhil-kumar <akillcoder@gmail.com>
2021-07-19 00:23:01 +0530

    db connection

--- connection/mongoose.js
+++ connection/mongoose.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-mongoose.connect('mongodb://localhost:27017/FinanceLoan', {                       
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-}).then(() => console.log('FinanceLoan DB connection successful!'));
+try {
+  const connectionURL = process.env.DBPATH || 'mongodb://localhost:27017/FinanceLoad'
+
+  mongoose.connect(connectionURL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }).then(() => console.log('FinanceLoan DB connection successful!'));
+} catch (error) {
+  console.log('Error connecting mongoDB server')
+}
+
 
 
 module.exports = mongoose
